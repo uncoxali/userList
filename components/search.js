1a594f580b100ac661cb8e1511afb801b975098b
Nameuncoxali <Emailalif.mohamady20@gmail.com>
2020-12-27 18:02:31 +0330

    done

--- components/search.js
+++ components/search.js
@@ -1,19 +1,47 @@
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
+//components
+import Search from './Search';
 
-const Search = (props) => {
+const Search = props => {
 
+    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        setFilteredCountries(
+            props.users.filter((user) =>
+                user.lastName.toLowerCase().includes(search.toLowerCase())
+            )
+        );
+    }, [search, props.users]);
 
 
     return (
-        <>
+        <div className="container">
             <div className="flexbox">
                 <div className="search">
                     <div>
-                        <input type="text" placeholder="Search . . ." required />
+                        <input type="text" placeholder="Search . . ." onChange={(e) => setSearch(e.target.value)} />
                     </div>
                 </div>
             </div>
-        </>
+            <ul className="wrapper">
+                {
+                    filteredCountries.map(user => {
+                        return (
+                            <li className="nav-link item" key={user.id}>
+                                <img className="avatar" src={user.picture} alt="Ash" />
+                                <Link href="/users/[id]" as={`/users/${user.id}`}>
+                                    <a className="link">{user.lastName}</a>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
